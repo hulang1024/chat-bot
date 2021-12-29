642bf627981d7e4508b5efe370f392d717e5b393
hulang1024 <1013644379@qq.com>
2021-12-29 22:21:36 +0800

    refactor

--- nlp/cut-word.rkt
+++ nlp/cut-word.rkt
@@ -6,150 +6,119 @@
          
 
 (define (print/join words)
-  (displayln (string-join words " / ")))
+  (displayln (string-join words "/" #:before-first "/" #:after-last "/")))
 
 
+; 基于词典的切分，双向最大匹配算法
 (define (cut-words/mm sentence)
   (set! sentence (string-trim sentence))
   (define len (string-length sentence))
+
+  ; reverse? #t为反向，#f为正向
+  (define (cut reverse?)
+    (define (read-section out pred i)
+      (define cs null)
+      (let loop ()
+        (define c (string-ref sentence i))
+        (when (pred c)
+          (cond
+            [reverse?
+             (set! cs (cons c cs))
+             (set! i (- i 1))
+             (when (>= i 0)
+               (loop))]
+            [else
+             (set! cs (append cs (cons c null)))
+             (set! i (+ i 1))
+             (when (< i len)
+               (loop))])))
+      (set-box! out (list->string cs))
+      i)
   
-  (define (fmm)
-    (define words null)
-    (define (add-word word)
-      (set! words (append words (cons word null))))
-    (define rest-start 0)
-    (define word-len 0)
-    (define sub null)
-    (let loop ()
-      (set! word-len (min dict-max-word-length (- len rest-start)))
-      (let loop ()
-        (set! sub (substring sentence rest-start (+ rest-start word-len)))
-        (cond
-          [(= word-len 1) (add-word sub)]
-          [(dict-has? sub) (add-word sub)]
-          [else
-           (set! word-len (- word-len 1))
-           (when (> word-len 0)
-             (loop))]))
-      (set! rest-start (+ rest-start word-len))
-      (when (< rest-start len)
-        (loop)))
-    words)
-	
-  (define (bmm)
-    (define words null)
-    (define (add-word word)
-      (set! words (cons word words)))
-    (define rest-end len)
-    (define word-len 0)
-    (define sub null)
-    (let loop ()
-      (set! word-len (min dict-max-word-length rest-end))
-      (let loop ()
-        (set! sub (substring sentence (- rest-end word-len) rest-end))
-        (cond
-          [(= word-len 1) (add-word sub)]
-          [(dict-has? sub) (add-word sub)]
-          [else
-           (set! word-len (- word-len 1))
-           (when (> word-len 0)
-             (loop))]))
-      (set! rest-end (- rest-end word-len))
-      (when (> rest-end 0)
-        (loop)))
-    words)
+    (define (read-number out i)
+      (read-section out char-numeric? i))
+  
+    (define (read-spaces out i)
+      (read-section out char-whitespace? i))
 
-  (define f-words (fmm))
-  (define b-words (bmm))
+    (define (read-alpha-word out i)
+      (read-section out alpha-word-char? i))
+
+    (define (alpha-word-char? c)
+      (or (char-upper-case? c) (char-lower-case? c)))
+
+    (define words null)
+    (define add-word
+      (if reverse?
+          (λ (word)
+            (set! words (cons word words)))
+          (λ (word)
+            (set! words (append words (cons word null))))))
+    (define start (if reverse? (- len 1) 0))
+    (define word-len 0)
+    (define sub null)
+
+    (let loop ()
+      (let loop-non-han ()
+        (define c (string-ref sentence start))
+        (define is-han #f)
+        (define out (box #f))
+        (match c
+          [(? char-numeric? c)
+           (set! start (read-number out start))]
+          [(? char-whitespace? c)
+           (set! start (read-spaces out start))]
+          [(? alpha-word-char? c)
+           (set! start (read-alpha-word out start))]
+          [_ (set! is-han #t)])
+        (when (unbox out)
+          (add-word (unbox out)))
+        (when (and (if reverse? (>= start 0) (< start len))
+                   (not is-han))
+          (loop-non-han)))
+
+      (when (if reverse? (>= start 0) (< start len))
+        (set! word-len (min dict-max-word-length (if reverse? (+ start 1) (- len start))))
+        (let loop ()
+          (set! sub (if reverse?
+                        (substring sentence (+ 1 (- start word-len)) (+ start 1))
+                        (substring sentence start (+ start word-len))))
+          (cond
+            [(= word-len 1) (add-word sub)]
+            [(dict-has? sub) (add-word sub)]
+            [else
+             (set! word-len (- word-len 1))
+             (when (> word-len 0)
+               (loop))]))
+        (set! start ((if reverse? - +) start word-len)))
+      (when (if reverse? (>= start 0) (< start len))
+        (loop)))
+    words)
+  
   (define (single-count words)
-    (count (λ (word)
-             (= (string-length word) 1))
+    (count (λ (word) (= (string-length word) 1))
            words))
+  
+  (define f-words (cut #f))
+  (define b-words (cut #t))
   (define f-total (length f-words))
-  (define b-total (length b-words))
-  (define words
-    (cond
-      [(= f-total b-total)
-       (cond
-         [(andmap string=? f-words b-words) f-words]
-         [else
-          (define f-cnt (single-count f-words))
-          (define b-cnt (single-count b-words))
-          (if (>= f-cnt b-cnt)
-              b-words
-              f-words)])]
-      [else
-       (if (> f-total b-total)
-           b-words
-           f-words)]))
-  (parse-words words))
-
-
-(define (parse-words words)
-  (define count (length words))
-  (define result null)
-  (define acc-word-chars null)
-
-  (define (add-word word)
-    (when (non-empty-string? word)
-      (set! result (append result (cons word null)))))
-
-  (define (acc-word-char c)
-    (set! acc-word-chars (append acc-word-chars (cons c null))))
-
-  (define (add-acc-word)
-    (unless (null? acc-word-chars)
-      (add-word (list->string acc-word-chars))
-      (set! acc-word-chars null)))
-
-  (define prev-is-digit #f)
-  (define prev-is-colon #f)
-  (define prev-is-space #f)
-  (define prev-is-letter #f)
-
-  (let loop ([i 0])
-    (define word (list-ref words i))
-    (define is-digit #f)
-    (define is-space #f)
-    (define is-colon #f)
-    (define is-letter #f)
-    (cond
-      [(= (string-length word) 1)
-       (define c (string-ref word 0))
-       (cond
-         [(char-numeric? c)
-          (set! is-digit #t)
-          (when (and (not prev-is-digit)
-                     (not prev-is-colon))
-            (add-acc-word))
-          (acc-word-char c)]
-         [(or (char=? c #\:) (char=? c #\:))
-          (set! is-colon #t)
-          (when (not prev-is-digit)
-            (add-acc-word))
-          (acc-word-char #\:)]
-         [(char-whitespace? c)
-          (set! is-space #t)
-          (when (not prev-is-space)
-            (add-acc-word))
-          (acc-word-char c)]
-         [(or (char-lower-case? c)
-              (char-upper-case? c))
-          (set! is-letter #t)
-          (when (not prev-is-letter)
-            (add-acc-word))
-          (acc-word-char c)]
-         [else
-          (add-acc-word)
-          (add-word word)])]
-      [else
-       (add-acc-word)
-       (add-word word)])
-    (set! prev-is-digit is-digit)
-    (set! prev-is-colon is-colon)
-    (set! prev-is-space is-space)
-    (set! prev-is-letter is-letter)
-    (when (< i (- count 1))
-      (loop (+ i 1))))
-  (add-acc-word)
-  result)
+  (define b-total (length b-words))
+  ; 对正向和反向的结果进行比较，使用启发式规则决定采用哪一种结果
+  (cond
+    ; 如果词数相同
+    [(= f-total b-total)
+     (cond
+       ; 如果分词内容相同，说明没有歧义，返回任意一个
+       [(andmap string=? f-words b-words) f-words]
+       [else
+        (define f-cnt (single-count f-words))
+        (define b-cnt (single-count b-words))
+        ; 否则，返回单字词较少的那个
+        (if (>= f-cnt b-cnt)
+            b-words
+            f-words)])]
+    [else
+     ; 否则，返回词数较少的那个
+     (if (> f-total b-total)
+         b-words
+         f-words)]))
