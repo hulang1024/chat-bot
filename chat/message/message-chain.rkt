21a544b01d990a82b30bcf56a4462c0bd946c823
hulang1024 <1013644379@qq.com>
2022-01-12 21:59:39 +0800

    update: 消息包长度优化

--- chat/message/message-chain.rkt
+++ chat/message/message-chain.rkt
@@ -9,32 +9,53 @@
   (class* object% (message<%>)
     (super-new)
 
-    (define lst null)
+    (define messages null)
     (define content-string null)
 
-    (define/public (to-list) lst)
+    (define/public (to-list) messages)
 
     (define/public (get p)
       (cond
-        [(number? p) (list-ref lst p)]
+        [(number? p) (list-ref messages p)]
         [(class? p) (get (is-a?/c p))]
         [(procedure? p)
-         (findf p lst)]))
+         (findf p messages)]))
 
-    (define/public (add m)
-      (when (string? m)
-        (set! m (if (string=? m "") #f (new plain% [text m]))))
-      (when m
-        (set! lst (append lst (cons m null)))
+    (define/public (add new-m)
+      (define to-add?
+        (cond
+          [(null? messages) #t]
+          [else
+           (cond
+             [(or (string? new-m)
+                  (is-a? new-m plain%)
+                  (is-a? new-m mirai-code-message%))
+              ; 将新文本合并到上个文本或code（也是一种文本)消息里
+              (define pre-m (last messages))
+              (cond
+                [(or (is-a? pre-m plain%)
+                     (is-a? pre-m mirai-code-message%))
+                 (define last-text (send pre-m get-text))
+                 (define new-text (if (string? new-m) new-m (send new-m get-text)))
+                 (send pre-m set-text (string-append last-text new-text))
+                 #f]
+                [else #t])]
+             [else #t])]))
+         
+      (when to-add?
+        (set! new-m (if (string? new-m)
+                        (new plain% [text new-m])
+                        new-m))
+        (set! messages (append messages (cons new-m null)))
         (set! content-string null)))
 
-    (define/public (empty?) (null? lst))
+    (define/public (empty?) (null? messages))
 
     (define/public (trim)
-      (define left (if (is-a? (list-ref lst 0) source%)
-                       (list-ref lst 1)
-                       (list-ref lst 0)))
-      (define right (last lst))
+      (define left (if (is-a? (list-ref messages 0) source%)
+                       (list-ref messages 1)
+                       (list-ref messages 0)))
+      (define right (last messages))
 
       (when (or (is-a? left plain%) (is-a? left mirai-code-message%))
         (define trimmed (string-trim (send left get-text) #:right? #f))
@@ -47,7 +68,7 @@
     (define/public (content-to-string)
       (when (null? content-string) 
         (define strs (filter (compose not false?)
-                             (map (λ (m) (send m content-to-string)) lst)))
+                             (map (λ (m) (send m content-to-string)) messages)))
         (set! content-string (string-join strs "")))
       content-string)))
 
