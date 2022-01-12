9704aa8f8574c128f3a1e4480d1f531d31706f3a
hul <1013644379@qq.com>
2022-01-12 10:15:58 +0800

    update(eval): 发送完消息后删图像文件

--- eval-service/lisp/handle-message.rkt
+++ eval-service/lisp/handle-message.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require "eval.rkt"
-         "../../chat/message/main.rkt")
+         "../../chat/message/main.rkt"
+         "../../chat/contact/message-send.rkt")
 
 (provide handle-message)
 
@@ -23,17 +24,23 @@
       (handle-api-result (eval-program expr-str
                                        "global"
                                        (send event get-sender))
+                         event
                          add-message
                          source-message
                          has-quote-reply?)
       #f))
 
 
-(define (handle-api-result api-result add-message source-message has-quote-reply?)
+(define (handle-api-result api-result event add-message source-message has-quote-reply?)
   (cond
     [(send api-result ok?)
+     (define mcb (new message-chain-builder%))
+     (define add-message (create-add-message mcb))
+
      (define value (send api-result get-value))
      (define output (send api-result get-output))
+     (define has-output (not (null? output)))
+
      (define (convert-message items)
        (for-each
         (λ (item)
@@ -42,19 +49,28 @@
              ["image" (new image-message% [path (hash-ref item 'path)])]
              [else (new mirai-code-message% [code (string-trim (hash-ref item 'content))])])))
         items))
-     
-     (define has-output (not (null? output)))
+
      (when has-quote-reply?
        (add-message (make-quote-reply source-message)))
      (convert-message output)
      (when (and (not (null? value))
                 (or (not has-output)
                     (not (match value
-                           [(hash-table ('type "text") ('content "#<void>")) #f]
+                           [(list (hash-table ('type "text") ('content "#<void>"))) #t]
                            [else #f]))))
        (when has-output
          (add-message "\n"))
-       (convert-message value))]
+       (convert-message value))
+
+     (define message-chain (send mcb build))
+     (message-receipt-promise-then
+      (send (send event get-subject) send-message message-chain)
+      (λ (_)
+        (for-each
+         (λ (m)
+           (when (and (is-a? m image-message%) (send m get-path))
+             (delete-file (send m get-path))))
+         (send message-chain to-list))))]
     [else
      (define error (send api-result get-error))
      (add-message (face-from-id 270))
